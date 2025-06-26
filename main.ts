bf2c79025bdbf4a46580f30d29a014abfc2b5066
hoang19020302 <tranichhoang2001@gmail.com>
2025-06-26 14:40:46 +0700

    test cron

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 import { Hono } from 'https://deno.land/x/hono@v4.3.5/mod.ts'
-
+import { scheduled } from './cron'
 const app = new Hono()
 
 app.get('/', (c) => c.text('Hello from Deno Deploy! 🎉'))
 
 // Bạn có thể thêm route khác như:
 app.get('/api/hello', (c) => c.json({ msg: "Hi! Deno Deploy! 🎉" }))
+
+Deno.cron("send main cron", "* * * * *", scheduled);
 
 Deno.serve(app.fetch)
